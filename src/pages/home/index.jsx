c2e74dd3fad9185f494edec5ae39f9818b3d6d24
Everton Silva <everton.p.silva@outlook.com>
2023-03-25 12:36:11 -0300

    Exemplos de redirecionamento de Página usando o React Router Dom

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,7 +1,13 @@
+import { Link, useNavigate } from "react-router-dom";
+
 export default function HomePage() {
+  const navigate = useNavigate();
+
   return (
     <div>
-      <h1>Home do EVertinho</h1>
+      <h1>Home do Evertinho</h1>
+      <button onClick={() => navigate("/forms")}>Editar</button>
+      <Link to="/forms">Editar</Link>
     </div>
   );
 }
