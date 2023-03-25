6be68d01d1cd1c0e670ecc5140fe246492e236ec
Everton Silva <everton.p.silva@outlook.com>
2023-03-25 12:48:12 -0300

    Obtendo uma lista de alunos

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,11 +1,38 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
 import { Link, useNavigate } from "react-router-dom";
 
+const URL_STUDENTS = "https://api-aluno.vercel.app/aluno";
+
 export default function HomePage() {
+  const [students, setStudents] = useState([]);
   const navigate = useNavigate();
+
+  useEffect(() => {
+    fetchStudents();
+  }, []);
+
+  async function fetchStudents() {
+    try {
+      const response = await axios.get(URL_STUDENTS);
+      setStudents(response.data);
+    } catch (error) {
+      alert("Não foi possível fazer a consulta");
+    }
+  }
 
   return (
     <div>
-      <h1>Home do Evertinho</h1>
+      <h1>Alunos Evertinho</h1>
+      <ul>
+        {students.map((student) => (
+          <li key={student._id}>
+            <h3>{student.nome}</h3>
+            <p>Curso: {student.curso}</p>
+            <p>Bimestre: {student.bimestre}</p>
+          </li>
+        ))}
+      </ul>
       <button onClick={() => navigate("/forms")}>Editar</button>
       <Link to="/forms">Editar</Link>
     </div>
